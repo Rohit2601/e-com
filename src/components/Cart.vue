d99aab220f71ae0571aca1f51355c5e5c67c4fbc
Rohit Bharmal <rbharmal@rbharmal-thinkpadt14sgen1.remote.csb>
2023-07-26 10:37:11 +0530

    files added

---
+++ src/components/Cart.vue
@@ -0,0 +1,36 @@
+<script>
+import "@patternfly/pfe-button";
+import "@patternfly/pfe-card";
+
+export default {
+  name: "Cart",
+  data() {},
+};
+</script>
+
+<template>
+  <div class="cart-body">
+    <div class="pfe-card-body">
+      <pfe-card>
+        <h2 slot="pfe-card--header">Your items in cart</h2>
+        <div>
+          <h1>Product Name:</h1>
+          <h2>Price:</h2>
+        </div>
+        <div>
+          <h1>edwdas</h1>
+        </div>
+      </pfe-card>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.pfe-card-body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  margin-top: 10%;
+}
+</style>
