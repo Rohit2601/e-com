02d7657dc100863bb4d237801548c503613ed077
Rohit Bharmal <rohitbharmal123@gmail.com>
2023-07-31 15:25:44 +0530

    Implimented store manager

--- src/components/ProductCategory.vue
+++ src/components/ProductCategory.vue
@@ -1,60 +1,65 @@
 <script setup>
 import "@patternfly/pfe-card";
-import { useRouter } from 'vue-router';
-import { ref, watch } from 'vue';
-import router from '../routes.js';
+import { useRouter } from "vue-router";
+import { ref, watch } from "vue";
+import router from "../routes.js";
 
 const routerUrl = useRouter();
 var data = [];
 var routeName = routerUrl.currentRoute.value.query.name;
-var pageStatus = ref('null');
+var pageStatus = ref("null");
 
 const categoryData = (category) => {
-  if (category == 'laptops') {
-    pageStatus.value = 'loading'
-    fetch('./LaptopData.json')
+  if (category == "laptops") {
+    pageStatus.value = "loading";
+    fetch("./LaptopData.json")
       .then((response) => response.json())
-      .then((json) => {      
+      .then((json) => {
         data = json;
-        pageStatus.value = 'fetched';
+        pageStatus.value = "fetched";
       })
       .catch((error) => {
         console.log(error);
-      })
-  }
-  else if (category == 'mobiles') {
-    pageStatus.value = 'loading'
-    fetch('./MobileData.json')
+      });
+  } else if (category == "mobiles") {
+    pageStatus.value = "loading";
+    fetch("./MobileData.json")
       .then((response) => response.json())
-      .then((json) => {      
+      .then((json) => {
         data = json;
-        pageStatus.value = 'fetched';
+        pageStatus.value = "fetched";
       })
       .catch((error) => {
         console.log(error);
-      })
-  }
-  else {
-    pageStatus.value = 'null';
+      });
+  } else {
+    pageStatus.value = "null";
     data = [];
   }
 };
-const productDetailPage = (product,id) => {
+const productDetailPage = (product, id) => {
   router.push(`/product-details?product=${product}&id=${id}`);
-
-}
+};
 categoryData(routeName);
-watch(() => router.currentRoute.value.query.name, (newValue) => {
-  routeName = newValue;
-  categoryData(routeName);
-})
+watch(
+  () => router.currentRoute.value.query.name,
+  (newValue) => {
+    routeName = newValue;
+    categoryData(routeName);
+  }
+);
 </script>
 
 <template>
   <div class="category-box-cards">
     <h1 v-show="pageStatus == 'null'">Sorry,No Data Found !!</h1>
     <h1 v-show="pageStatus == 'loading'">Loading....</h1>
-    <pfe-card v-show="pageStatus == 'fetched'" class="category-card" v-for="(object,index) in data" @click="productDetailPage(routeName,object.id)">
+    <pfe-card
+      v-show="pageStatus == 'fetched'"
+      class="category-card"
+      v-for="(object, index) in data"
+      @click="productDetailPage(routeName, object.id)"
+    >
       <img :src="object.image" alt="image" class="category-card-image" />
       <div slot="pfe-card--footer">
         <h3>{{ object.modelName }}</h3>
@@ -73,7 +78,7 @@ watch(() => router.currentRoute.value.query.name, (newValue) => {
   align-items: center;
   flex-wrap: wrap;
 }
-.category-box-cards>h1 {
+.category-box-cards > h1 {
   height: 40vh;
 }
 .category-card {
@@ -88,7 +93,3 @@ watch(() => router.currentRoute.value.query.name, (newValue) => {
   width: 100%;
 }
 </style>
-
-
-
-
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './routes';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./routes";
+import store from "./store";
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.mount("#app");
+// app.provide("store", store);
